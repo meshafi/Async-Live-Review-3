aae5c3655bb3e3e08a88671867fea5ec2ae9b7e8
Pramod L <pramodliv1@gmail.com>
2023-10-30 10:15:05 +0530

    Fix sample fetch url

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 const fetch = require("node-fetch");
 
 async function main() {
-  const response = await fetch("http://localhost:3000/initialize");
+  const response = await fetch("http://localhost:3000");
   const data = await response.json();
   console.log(data);
 }
