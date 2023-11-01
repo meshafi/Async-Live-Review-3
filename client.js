10d52d0db5aecceb3f7aca2f189b5c43747bf0a9
meshafi <shafiaqueel@gmail.com>
2023-11-01 18:12:18 +0530

    added all the files of async live reveiw 3

--- client.js
+++ client.js
@@ -1,8 +1,33 @@
-const fetch = require("node-fetch");
+
+async function fetchRootDir(){
+   const res=await fetch("http://localhost:3000/");
+   const data=await res.json();
+   return data;
+}
+
+async function getFiles(data){
+  const promiseArray=[];
+   for(const obj of data.items){
+     if(obj.isDir==true){
+      const res=await fetch(`http://localhost:3000/${obj.name}`);
+      const dir=await res.json();
+      promiseArray.push(dir);
+     }
+   }
+
+   return Promise.all(promiseArray);
+}
 
 async function main() {
-  const response = await fetch("http://localhost:3000");
-  const data = await response.json();
-  console.log(data);
+  try{
+   const data=await fetchRootDir();
+   let fileData=await getFiles(data);
+  console.log(JSON.stringify(fileData,null,2));
+  }
+  catch(error){
+    console.log('Error occured',error);
+  }
 }
 main();
+
+
